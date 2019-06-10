02c1437f9f771812821616f10b08601944b0b6d3
Philip Andes <philip__andes@hotmail.nl>
2019-06-10 16:35:39 +0200

    Clock finished

--- js/app.js
+++ js/app.js
@@ -1,11 +1,33 @@
 const secondHand = document.querySelector('.second-hand');
 
+const minuteHand = document.querySelector('.min-hand');
+
+const hourHand = document.querySelector('.hour-hand');
+
 function setDate() {
   const now = new Date();
-  const seconds = now.getSeconds();
-  const secondsDegrees = ((seconds / 60) * 360) + 90;
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-  console.log(seconds);
+  const second = now.getSeconds();
+  const secondDegrees = ((second / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+
+  const minute = now.getMinutes();
+  const minuteDegrees = ((minute / 60) * 360) + 90;
+  minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
+
+  const hours = now.getHours();
+  const hourDegrees = ((hours / 12) * 360) + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
+
+// function setDate2() {
+//   const now = new Date();
+//   const minutes = now.getMinutes();
+//   const minutesDegrees = ((minutes / 60) * 360) + 90;
+//   minuteHand.style.transfrom = `rotate(${minutesDegrees}deg)`;
+//   console.log(minutes);
+// }
+
+// setInterval(setDate2, 1000);
+
